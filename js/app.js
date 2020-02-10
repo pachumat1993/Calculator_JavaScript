c49cd594e9f47d8493de99be1f186582df63c8f0
Mateusz Pachuta <matpac2493@gmail.com>
2020-02-10 19:38:20 +0100

    Określenie warotści stałych, model DOM HTML5, pobranie stałych wartosci, które się nie zmieniają, czyli button dodaj, odejmij, pomnóż podziel

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 // 1 sprawdzenie czy skrypt  działa prawidłowo 
 console.log("Skrypt jest podpięty--działa prawidłowo")
+
+// 2 Określenie wartości stałych :
+// pobranie stałych, wartości które się nie zmieniają, czyli button dodaj, odejmij, pomnóż, podziel, pierwiastek, wynik, wyczysc
+
+const buttonAddition = document.getElementById("btn-add")
+const buttonSubstraction = document.getElementById("btn-subtract")
+const buttonMultiplication = document.getElementById("btn-multiply")
+const buttonDivison = document.getElementById("btn-divide")
+const buttonSquareRoot= document.getElementById("btn-squareRoot")
+const curentResult = document.getElementById("current-result")
+const buttonClear = document.getElementById("btn-clear")
